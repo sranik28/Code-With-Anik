dc7d4dc3fbd5b7f746b53bcf5e4630e3c2855ad4
Shahriar Rahman Anik <122194751+sranik28@users.noreply.github.com>
2024-12-21 22:34:50 +0600

    auth

--- src/app/middlewares/Auth.ts
+++ src/app/middlewares/Auth.ts
@@ -1,15 +1,55 @@
-import { NextFunction, Request, Response } from 'express';
-import { TUserRole } from '../modules/User/user.interface';
-import { CatchAsync } from '../utils/CatchAsync';
-import { AppError } from '../errors/AppErrors';
+// import { NextFunction, Request, Response } from 'express';
+// import { TUserRole } from '../modules/User/user.interface';
+// import { CatchAsync } from '../utils/CatchAsync';
+// import { AppError } from '../errors/AppErrors';
 
-export const auth = (...requiredRoles: TUserRole[]) => {
-  return CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+// export const auth = (...requiredRoles: TUserRole[]) => {
+//   return CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
+//     const token = req.headers.authorization;
+//     if (!token) {
+//       throw new AppError(401, 'You are not logged in');
+//     }
+//   });
+// };
+
+import { TUserRole } from "../modules/User/user.interface";
+import { User } from "../modules/User/user.model";
+import { CatchAsync } from "../utils/CatchAsync";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+
+const auth = (requireRoles: TUserRole[]) => {
+  return CatchAsync(async (req, res, next) => {
     const token = req.headers.authorization;
+    
+
     if (!token) {
-      throw new AppError(401, 'Token not found');
+      throw new Error("You are not Authorized!");
     }
+
+    const decode = jwt.verify(token?.split(" ")[1], "secret") as JwtPayload;
+    console.log("decode -------->", decode);
+
+    
+
+    const { email, role, name } = decode;
+    console.log(name);
+    
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User not found!");
+    }
+    console.log(requireRoles, role);
+    
+
+    if (requireRoles && !requireRoles.includes(role)) {
+      throw new Error("Your role not match so, You are not Authorized!");
+    }
+    
+    next();
   });
 };
 
+export default auth;
 
--- src/app/modules/Blog/blog.router.ts
+++ src/app/modules/Blog/blog.router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { BlogControllers } from './blog.controller';
-import { auth } from '../../middlewares/Auth';
+import  auth  from '../../middlewares/Auth';
 import { USER_ROLE } from '../User/user.constant';
 import { BlogValidation } from './blog.validation';
 
@@ -9,11 +9,11 @@ const router = Router();
 
 router.post(
   '/',
-  auth(USER_ROLE.user),
+  auth([USER_ROLE.user]),
   validateRequest(BlogValidation.BlogValidationSchema),
   BlogControllers.createBlog,
 );
-router.get('/', auth(USER_ROLE.user), BlogControllers.getAllBlogs);
-router.get('/:id', auth(USER_ROLE.user), BlogControllers.getSingleBlog);
+router.get('/', auth([USER_ROLE.user]), BlogControllers.getAllBlogs);
+router.get('/:id', auth([USER_ROLE.user]), BlogControllers.getSingleBlog);
 
 export const BlogRouters = router;
