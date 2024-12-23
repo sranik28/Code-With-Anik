ae16657bc910a303d1191821e6f58e4423f88d42
Shahriar Rahman Anik <122194751+sranik28@users.noreply.github.com>
2024-12-24 01:24:12 +0600

    search

--- src/app/modules/Blog/blog.service.ts
+++ src/app/modules/Blog/blog.service.ts
@@ -2,16 +2,50 @@ import { Types } from 'mongoose';
 import { TBlog } from './blog.interface';
 import { Blog } from './blog.model';
 import { AppError } from '../../errors/AppErrors';
+import auth from '../../middlewares/Auth';
 
 const createBlogInToDB = async (payload: TBlog) => {
   const result = await Blog.create(payload);
   return result;
 };
 const getAllBlogsFromDB = async (query: Record<string, unknown>) => {
-  const result = await Blog.find(query).populate('author');
-  return result;
+  console.log('base query', query);
 
-  
+  const queryObj = { ...query };
+
+  const blogSearchableFields = ['title'];
+  let searchTerm = '';
+
+  if (query.searchTerm) {
+    searchTerm = query.searchTerm as string;
+  }
+
+  const searchQuery = Blog.find({
+    $or: blogSearchableFields.map((field) => ({
+      [field]: { $regex: searchTerm, $options: 'i' },
+    })),
+  });
+
+  const excludeField = ['search', 'sortBy', 'sortOrder'];
+  excludeField.forEach((el) => delete queryObj[el]);
+
+
+  // if (query?.filter) {
+  //   const authorId = query.filter as string;
+  //   if (Types.ObjectId.isValid(authorId)) {
+  //     queryObj.author = new Types.ObjectId(authorId);
+  //   } else {
+  //     throw new Error('Invalid author ID');
+  //   }
+  // }
+
+  const filterQuery = searchQuery.find(queryObj);
+
+  const sortBy = (query?.sortBy as string) || 'createdAt';
+  const sortOrder = query?.sortOrder === 'desc' ? -1 : 1;
+  const sortedQuery = filterQuery.sort({ [sortBy]: sortOrder });
+
+  return sortedQuery;
 };
 const getSingleBlogFromDB = async (id: string) => {
   const result = await Blog.findById(id).populate('author');
